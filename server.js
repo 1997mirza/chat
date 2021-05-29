76bf4f0b7e653d43da0835f5203e46386a2981e9
1997mirza <1997mirza@gmail.com>
2021-05-29 17:49:26 +0200

    Postavka projekta

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=> {
+    res.send('radiiii') 
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
