66c997b0b6f70b6924853971427ca14832641367
1997mirza <1997mirza@gmail.com>
2021-05-29 23:12:26 +0200

    prototip

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const path = require('path');
-const express = require('express');
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const {
+    PORT = 3000,
+    NODE_ENV = "development"
+} = process.env;
+const IN_PROD = NODE_ENV == "production" // za do
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
 
-app.get('/',(req,res)=> {
-    res.send('radiiii') 
+app.use(express.static(path.join(__dirname,'public')))
+
+// slusam na konekciju klijenta, odgovaram pozdravnom porukom
+io.on('connection',scoket => {
+    console.log('kova konekcija klijenta')
+    scoket.emit('pozdrav','pozdrav')
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+server.listen(PORT,() => {
+    console.log('slusam na 3000');
 })
