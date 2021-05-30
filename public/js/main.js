505737e1a0c222c8c53381ab3d61d87beda8c935
1997mirza <1997mirza@gmail.com>
2021-05-31 01:13:13 +0200

    chat, sidebar sa aktivnim korisnicima

--- public/js/main.js
+++ public/js/main.js
@@ -1,50 +1,107 @@
+const socket = io();
+const chatMessages = document.querySelector('.chat-messages')
+const chatFrom = document.querySelector('.btn-send')
+let listOfActiveUsers = document.querySelector('#users')
+
+chatFrom.addEventListener('click', (e) => {
+    e.preventDefault();
+    const msg = document.querySelector('#msg').value
+    document.querySelector('#msg').value = ""
+    document.querySelector('#msg').focus();
+    socket.emit('chatMessage', msg)
+})
+socket.on('msg', msg => {
+    console.log(msg)
+    outputMessage(msg)
+})
+socket.on('WelcomeMessage', msg => {
+    outputMessage(msg)
+    socket.emit('makeMeActive', localStorage.getItem("nick"))
+
+})
+socket.on('newActiveUser', msg => {
+    if (msg !== localStorage.getItem('nick')){
+    msgToPrint = "Korisnik " + msg + " se pridružio razgovoru"
+    outputMessage(msgToPrint)
+} 
+    
+})
+socket.on('updatedList', list => {
+    console.log(list)
+    resetSidebar(list)
+})
+socket.on('chat', msg => {
+    console.log(msg)
+
+})
+function outputMessage(msg) {
+    const div = document.createElement('div')
+    div.classList.add('message')
+    div.innerHTML = `
+    <p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${msg}
+    </p>
+   `
+    document.querySelector('.chat-messages').appendChild(div)
+    chatMessages.scrollTop = chatMessages.scrollHeight
+}
+function resetSidebar(list) {
+    console.log(list)
+    let listUsers = ``
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] == localStorage.getItem('nick')) continue
+        listUsers += `
+        <li>${list[i]}</>
+        `
+    }
+    listOfActiveUsers.innerHTML = listUsers;
+
+}
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.querySelector('.trigger-text').style.display = "none";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.querySelector('.trigger-text').style.display = "block";
 
-  }
-  function getNick() {
-		if (localStorage.getItem("nick") === null) {
-			generateNick();
-		} else {
-			document.querySelector('#nik').innerHTML = localStorage.getItem("nick")
-		}
-	}
-	function generateNick() {
-		const nick = [];
-		const suglasnici='bcdfghjklmnpqrstvwxyz';
-		const samoglasnici='aeiou';
-		const brojevi='0123456789';
-		for (let i = 0; i < 6; i++) {
-			if (i==0 || i==2){
-				nick.push(suglasnici.charAt(Math.floor(Math.random() * 
- 				suglasnici.length)));
-			} else if (i==1 || i==3) {
-				nick.push(samoglasnici.charAt(Math.floor(Math.random() * 
- 				samoglasnici.length)));
-			} else {
-				nick.push(brojevi.charAt(Math.floor(Math.random() * 
- 				brojevi.length)));
-			}
-		}
-		document.querySelector('#nik').innerHTML = nick.join('');
-		localStorage.setItem('nick', nick.join(''));
+}
+function getNick() {
+    if (localStorage.getItem("nick") === null) {
+        generateNick();
+    } else {
+        document.querySelector('#nik').innerHTML = localStorage.getItem("nick")
+    }
+}
+function generateNick() {
+    const nick = [];
+    const suglasnici = 'bcdfghjklmnpqrstvwxyz';
+    const samoglasnici = 'aeiou';
+    const brojevi = '0123456789';
+    for (let i = 0; i < 6; i++) {
+        if (i == 0 || i == 2) {
+            nick.push(suglasnici.charAt(Math.floor(Math.random() *
+                suglasnici.length)));
+        } else if (i == 1 || i == 3) {
+            nick.push(samoglasnici.charAt(Math.floor(Math.random() *
+                samoglasnici.length)));
+        } else {
+            nick.push(brojevi.charAt(Math.floor(Math.random() *
+                brojevi.length)));
+        }
+    }
+    document.querySelector('#nik').innerHTML = nick.join('');
+    localStorage.setItem('nick', nick.join(''));
 
-	}
-	function showChat() {
-		document.querySelector('#pocetna').style.display="none";
-		document.querySelector('#chat').style.display="block";
+}
+function showChat() {
+    document.querySelector('#pocetna').style.display = "none";
+    document.querySelector('#chat').style.display = "block";
 
-	}
-    const socket = io();
+}
 
-    socket.on('pozdrav',pozdrav => {
-        console.log('pozdrav')
-    })
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const mongojs = require('mongojs')
+const { Socket } = require('dgram')
+const { count } = require('console')
 const {
     PORT = 3000,
     NODE_ENV = "development"
@@ -10,15 +13,77 @@ const IN_PROD = NODE_ENV == "production" // za do
 const app = express();
 const server = http.createServer(app)
 const io = socketio(server)
+const db = mongojs('chat', ['otvoreni'])
+let activeUsersList = []
+let arrayToFilter = [];
 
-app.use(express.static(path.join(__dirname,'public')))
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 // slusam na konekciju klijenta, odgovaram pozdravnom porukom
-io.on('connection',scoket => {
-    console.log('kova konekcija klijenta')
-    scoket.emit('pozdrav','pozdrav')
+io.on('connection', socket => {
+
+    db.otvoreni.find({}, (err, messages) => {
+        io.emit('chat', messages)
+    })
+    socket.emit('WelcomeMessage', 'Dobro došli na chat!')
+    socket.on('makeMeActive', user => {
+        let userToList = { "nick": user, "id": socket.id }
+        activeUsersList.push(userToList)
+        let counter = 0;
+        activeUsersList.forEach(element => {
+            if(element.nick === userToList.nick) counter++
+        });
+        if(counter == 1) 
+        {
+            socket.broadcast.emit('newActiveUser', user)
+        arrayToFilter = []
+        for (let i = 0; i < activeUsersList.length; i++) {
+            arrayToFilter.push(activeUsersList[i].nick)
+        }
+        uniqueArray = removeDuplicates(arrayToFilter)
+        console.log(uniqueArray)
+        io.emit('updatedList', uniqueArray)
+        }
+    })
+    // when client disc.
+    socket.on('disconnect', () => {
+        let userForOut = ""
+        let counter = 0;
+        for (let i = 0; i < activeUsersList.length; i++) {
+            if (socket.id === activeUsersList[i].id) userForOut = activeUsersList[i]
+        }
+        activeUsersList = removeDuplicates1(activeUsersList,userForOut)
+
+        for (let i = 0; i < activeUsersList.length; i++) {
+            if (userForOut.nick === activeUsersList[i].nick) counter++
+        }
+        if(counter == 0) {
+            let index = uniqueArray.indexOf(userForOut.nick)
+            uniqueArray.splice(index,1)
+            io.emit('updatedList', uniqueArray)
+        }
+       
+    })
+    //Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        db.otvoreni.insert({ "posiljalac": 'Ivo', 'vrijeme': "20:20", "poruka": msg }, (err, messages) => {
+            io.emit('msg', msg)
+        })
+    })
 })
 
-server.listen(PORT,() => {
+server.listen(PORT, () => {
     console.log('slusam na 3000');
 })
+
+function removeDuplicates(data) {
+    return data.filter((value,index) => data.indexOf(value) === index)
+}
+function removeDuplicates1(data,za) {
+    let newArray = [];
+    newArray = data.filter(el => {
+        return el.id != za.id
+    })
+    return newArray
+}
